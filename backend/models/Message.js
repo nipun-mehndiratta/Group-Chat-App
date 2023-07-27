83b0942a1a0505ed6505a1fddd2483f1896fc60b
nipun-mehndiratta <123867733+nipun-mehndiratta@users.noreply.github.com>
2023-07-27 11:12:50 +0530

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const messageSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   text: { type: String, required: true },
